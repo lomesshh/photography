67de9b5fe685b8c5350e69313097bb08a62cf444
lomesshh <78649480+lomesshh@users.noreply.github.com>
2021-03-17 15:59:59 +0530

    added another blog

--- photography-web/src/app/app-routing.module.ts
+++ photography-web/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashComponent } from './dash/dash.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'dash',component:DashComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
